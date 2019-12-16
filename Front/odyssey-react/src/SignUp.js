abce4834e97313ae33f212b4a23ac55cf62ec904
ines <inesqtr@gmail.com>
2019-12-16 17:48:45 +0000

    added form

--- Front/odyssey-react/src/SignUp.js
+++ Front/odyssey-react/src/SignUp.js
@@ -4,32 +4,95 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ''
+            name: '',
+            lastname: '',
+            email: '',
+            password: '',
+            passwordbis: ''
         }
-        this.updateEmailField = this.updateEmailField.bind(this);
+        this.updateDataField = this.updateDataField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    updateEmailField(e) {
+    updateDataField = (e) => {
         this.setState({
-            email: e.target.value,
+            [e.target.name]: e.target.value,
         });
+    }
+
+    handleSubmit = (e) => {
+        console.log(this.state);
+        e.preventDefault();
     }
 
 
 
 
     render() {
-        const { email } = this.state;
+        const { name, lastname, email, password, passwordbis } = this.state;
 
         return (
-            <div>
-                <h1>{email ? email : 'Write your email here'}</h1>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={this.updateEmailField}
-                    value={email}
-                />
+            <div className="Form">
+                <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-data">
+                        <label>Your Name:</label>
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={this.updateDataField}
+                            value={name}
+                        />
+                    </div>
+                    <div className="form-data">
+                        <label>Your lastname:</label>
+                        <input
+                            type="text"
+                            name="lastname"
+                            onChange={this.updateDataField}
+                            value={lastname}
+                        />
+                    </div>
+                    <div className="form-data">
+                        <label>Your Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={this.updateDataField}
+                            value={email}
+                        />
+                    </div>
+                    <div className="form-data">
+                        <label>Your Password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={this.updateDataField}
+                            value={password}
+                        />
+                    </div>
+                    <div className="form-data">
+                        <label>Repeat Your Password:</label>
+                        <input
+                            type="password"
+                            name="passwordbis"
+                            onChange={this.updateDataField}
+                            value={passwordbis}
+                        />
+                    </div>
+                    <div className="form-data">
+                        <input type="submit" value="Submit" />
+                    </div>
+
+
+
+
+
+
+
+
+                </form>
+
             </div>
         )
     }
