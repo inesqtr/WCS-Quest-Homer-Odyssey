105d225fdca7ec5f0baa7feb0b8cd81cc36f1f95
ines <inesqtr@gmail.com>
2019-12-27 21:47:41 +0000

    first client-server connection

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -7,12 +7,10 @@ const connection = require('../../helpers/db.js');
 router.post('/signup', function (req, res, next) {
     console.log(req.body)
     const query = connection.query('INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)', [req.body.email, req.body.password, req.body.name, req.body.lastname], function (err, results, fields) {
-        if (err) {
-            console.log(err);
-            res.status(500).end("There was a SQL error!");
-        } else {
-            res.send("User added!");
-        }
+        if (err)
+            res.status(500).json({ flash: err.message });
+        else
+            res.status(200).json({ flash: "User has been signed up!" });
     });
 });
 
